b564cdf151b82f252c1fa7f1f0f2762d99bbd2fb
SerVitasik <87546293+SerVitasik@users.noreply.github.com>
2023-04-04 23:16:42 +0300

    new expense was added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     { id: 'e2', title: 'Car Insurance2', amount: 2294.67, date: new Date(2022, 3, 25) },
     { id: 'e3', title: 'Car Insurance3', amount: 3294.67, date: new Date(2023, 3, 25) },
     { id: 'e4', title: 'Car Insurance4', amount: 4294.67, date: new Date(2023, 3, 25) },
+    { id: 'e5', title: 'Car Insurance6', amount: 5294.67, date: new Date(2022, 1, 25) },
   ];
   console.log(expenses[0].date);
 
